17c8f0bab418393271f546a28dabfb5f8e22ebab
Dario Chamorro <dario.chamorro.v@gmail.com>
2015-12-01 09:43:58 -0500

    Initial Commit

---
+++ Universales/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Universales
+//
+//  Created by Aplimovil on 12/1/15.
+//  Copyright © 2015 Aplimovil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
